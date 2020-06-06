47b42f05a6d22435004be08cabe223c1ecea21e7
kailaix <kailaix@hotmail.com>
2020-06-05 22:59:21 -0700

    update

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
                    'Topic :: Scientific/Engineering'],
     cmdclass = {'sdist': sdist_git},
     install_requires = install_requires,
